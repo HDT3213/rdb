e498315f16c466116e776c02c6675a79e89db068
hdt3213 <hdt3213@outlook.com>
2023-04-16 20:10:15 +0800

    add windows build script

--- build.sh
+++ build.sh
@@ -2,3 +2,4 @@
 
 CGO_ENABLED=0  GOOS=linux GOARCH=amd64 go build -o target/rdb-linux-amd64 ./
 go build -o target/rdb-darwin ./
+CGO_ENABLED=0  GOOS=windows GOARCH=amd64 go build -o target/rdb-windows-amd64.exe ./
