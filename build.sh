97f03c6b5d27dd955bb05114cb76478b44859beb
hdt3213 <hdt3213@outlook.com>
2023-06-10 21:14:23 +0800

    update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 CGO_ENABLED=0  GOOS=linux GOARCH=amd64 go build -o target/rdb-linux-amd64 ./
-go build -o target/rdb-darwin ./
+CGO_ENABLED=0  GOOS=darwin GOARCH=amd64 go build -o target/rdb-darwin-amd64 ./
+CGO_ENABLED=0  GOOS=darwin GOARCH=arm64 go build -o target/rdb-darwin-arm64 ./
 CGO_ENABLED=0  GOOS=windows GOARCH=amd64 go build -o target/rdb-windows-amd64.exe ./
